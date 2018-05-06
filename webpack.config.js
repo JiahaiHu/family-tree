eabbb69ac0786abe649bc0f006fac1d6e259cb56
Hiko <johanhu2016@gmail.com>
2018-05-06 20:35:53 +0800

    add oss and yarn

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
         main: './static/scripts/index.js'
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'build.js'
     },
     module: {
