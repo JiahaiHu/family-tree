562606a3fcb7e79dedc9e643cb49eff75dc7e116
Hiko <johanhu2016@gmail.com>
2018-07-10 23:59:57 +0800

    add filter

--- src/scripts/Home.js
+++ src/scripts/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Checkbox, Row, Col } from 'antd';
+import { Layout, Checkbox, Row, Col, Icon } from 'antd';
 import styles from '../styles/Home.less';
 import classnames from 'classnames';
 const { Header, Content, Footer, Sider } = Layout
@@ -9,23 +9,69 @@ class Home extends React.Component {
     super(props);
     this.state = {
       collapsed: false,
+      checkedGroups: [],
+      checkedYears: [],
+      checkedGenders: [],
     };
   }
 
+  leftHandler = () => {
+    this.setState({
+      collapsed: true,
+    });
+  }
+
+  rightHandler = () => {
+    this.setState({
+      collapsed: false,
+    });
+  }
+
+  groupCheckHandler = (checkedValue) => {
+    this.setState({
+      checkedGroups: checkedValue,
+    })
+  }
+
+  yearCheckHandler = (checkedValue) => {
+    this.setState({
+      checkedYears: checkedValue,
+    })
+  }
+
+  genderCheckHandler = (checkedValue) => {
+    this.setState({
+      checkedGenders: checkedValue,
+    })
+  }
+
   render() {
+    const collapsedWidth = 160;
     return (
       <Layout className={classnames(styles.wrapper, styles.home)}>
-        <Header>
+        <Header className={styles.homeHeader}>
           Family Tree
         </Header>
         <Layout>
-          <Sider theme={'light'} collapsible={true} collapsedWidth={100}>
-            <header>
-              Group
-            </header>
-            <Row>
-              <Col span={12}>
-                <Checkbox.Group>
+          <Sider
+            theme={'light'}
+            className={styles.filter}
+            width={collapsedWidth * 2}
+            collapsible
+            collapsed={this.state.collapsed}
+            collapsedWidth={collapsedWidth}
+            trigger={null}
+          >
+            <div className={styles.filterHeader}>
+              <span>Group</span>
+              <div className={styles.trigger}>
+                <Icon type="left" onClick={this.leftHandler} style={{ opacity: this.state.collapsed ? 0.2 : 0.8 }} />
+                <Icon type="right" onClick={this.rightHandler} style={{ opacity: this.state.collapsed ? 0.8 : 0.2 }} />
+              </div>
+            </div>
+            <div className={styles.filterContent}>
+              <div>
+                <Checkbox.Group onChange={this.groupCheckHandler}>
                   <Row>
                     <Checkbox value="Android" className={styles.Android}>Android</Checkbox>
                   </Row>
@@ -51,12 +97,41 @@ class Home extends React.Component {
                     <Checkbox value="Web" className={styles.Web}>Web</Checkbox>
                   </Row>
                 </Checkbox.Group>
-              </Col>
-              <Col span={12}>
-                <header>
+              </div>
+              <div className={styles.extendedFilter}>
+                <div>
                   Generation Year
-                </header>
-                <Checkbox.Group>
+                </div>
+                <Checkbox.Group onChange={this.yearCheckHandler}>
+                  <Row>
+                    <Checkbox value="2011">2011</Checkbox>
+                  </Row>
+                  <Row>
+                    <Checkbox value="2012">2012</Checkbox>
+                  </Row>
+                  <Row>
+                    <Checkbox value="2013">2013</Checkbox>
+                  </Row>
+                  <Row>
+                    <Checkbox value="2014">2014</Checkbox>
+                  </Row>
+                  <Row>
+                    <Checkbox value="2015">2015</Checkbox>
+                  </Row>
+                  <Row>
+                    <Checkbox value="2016">2016</Checkbox>
+                  </Row>
+                  <Row>
+                    <Checkbox value="2017">2017</Checkbox>
+                  </Row>
+                  <Row>
+                    <Checkbox value="2018">2018</Checkbox>
+                  </Row>
+                </Checkbox.Group>
+                <div>
+                  Gender
+                </div>
+                <Checkbox.Group onChange={this.genderCheckHandler}>
                   <Row>
                     <Checkbox value="Male">Male</Checkbox>
                   </Row>
@@ -64,8 +139,8 @@ class Home extends React.Component {
                     <Checkbox value="Female">Female</Checkbox>
                   </Row>
                 </Checkbox.Group>
-              </Col>
-            </Row>
+              </div>
+            </div>
           </Sider>
           <Content>main content</Content>
           <Sider>right sidebar</Sider>
