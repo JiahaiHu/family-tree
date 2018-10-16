6a3fea1222fe249da179cbe269a13b6d91ac4edd
Hiko <johanhu2016@gmail.com>
2018-10-16 21:18:17 +0800

    add upload

---
+++ src/scripts/Upld.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Upload, Button } from 'antd'
+
+class Upld extends React.Component {
+
+  render() {
+    const props = {
+      action: 'https://fmt.fredliang.cn/files',
+      beforeUpload: (file) => {
+        // TODO: limit file size or type
+
+        // get policy and signature
+        const param = 'table=user&table_id=1&field=avatar&action=add'
+        fetch(`https://fmt.fredliang.cn/files/token?${param}`, {
+          method: 'GET',
+        })
+        .then(res => res.json())
+        .then(data => {
+          // upload to oss
+          const formData = new FormData()
+          if (data) {
+            Object.keys(data).map(key => {
+              formData.append(key, data[key])
+            })
+          }
+          
+          formData.append(file.filename, file)
+          fetch('https://fmt.hustunique.com', {
+            method: 'POST',
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: Object.assign({}, formData, data),
+          }).then()
+        })
+
+        return false  // cancel uploading
+      },
+      customRequest: ({
+        action,
+        data,
+        file,
+        filename,
+        headers,
+        onError,
+        onProgress,
+        onSuccess,
+        withCredentials,
+      }) => {
+        
+
+        
+      },
+    }
+    return (
+      <Upload {...props} >
+        <Button>上传</Button>
+      </Upload>
+    )
+  }
+}
+
+export default Upld;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,6 +10,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import Login from './Login'
 import Home from './Home'
 import User from './User'
+import Upld from './Upld'
 
 const endpoint = 'https://fmt.fredliang.cn/graphql'
 
@@ -40,6 +41,7 @@ class App extends React.Component {
       <ApolloProvider client={client}>
         <BrowserRouter>
           <Switch>
+            <Route path="/upload" component={Upld} />
             <Route path="/user" component={User} />
             <Route path="/home" component={Home} />
             <Route path="/" component={Login} />
